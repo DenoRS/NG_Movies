ac14068079393b6c8753d8411fa853f3732d8a40
Dennis Petcu <dennispetcu@gmail.com>
2025-09-29 11:31:32 +0100

    feature/unit_tests: add unit tests for domain layer

---
+++ domain/src/test/java/ie/rubberduck/domain/usecase/GetPopularMoviesUseCaseTest.kt
@@ -0,0 +1,102 @@
+package ie.rubberduck.domain.usecase
+
+import ie.rubberduck.domain.models.MovieModel
+import ie.rubberduck.domain.repository.MovieRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class GetPopularMoviesUseCaseTest {
+
+    private lateinit var classUnderTest: GetPopularMoviesUseCase
+
+    private val repository: MovieRepository = mockk()
+
+    private val testDispatcher = StandardTestDispatcher()
+    private val testScope = TestScope(testDispatcher)
+
+    @BeforeEach
+    fun setUp() {
+        classUnderTest = GetPopularMoviesUseCase(repository, testDispatcher)
+    }
+
+    @Test
+    @DisplayName(
+        """
+            GIVEN a valid page number
+            WHEN I invoke this use case
+            THEN I expect a list of most popular movies to be returned
+        """
+    )
+    fun testReturnsMostPopularMovies() = testScope.runTest {
+        // GIVEN
+        val page = 1
+        val expectedMovies = listOf(
+            MovieModel(
+                id = 101,
+                title = "The Dark Knight",
+                releaseDate = "2008-07-18",
+                posterPath = "/batman.jpg",
+                overview = "Batman vs Joker",
+                voteAverage = 9.0
+            )
+        )
+        coEvery { repository.getPopularMovies(page) } returns expectedMovies
+
+        // WHEN
+        val actualMovies = classUnderTest(page).first()
+
+        // THEN
+        assertEquals(expectedMovies, actualMovies)
+    }
+
+    @Test
+    @DisplayName(
+        """
+            GIVEN a valid page number but no results
+            WHEN I invoke this use case
+            THEN I expect an empty list to be returned
+        """
+    )
+    fun testReturnsEmptyList() = testScope.runTest {
+        // GIVEN
+        val page = 2
+        coEvery { repository.getPopularMovies(page) } returns emptyList()
+
+        // WHEN
+        val actualMovies = classUnderTest(page).first()
+
+        // THEN
+        assertEquals(emptyList<MovieModel>(), actualMovies)
+    }
+
+    @Test
+    @DisplayName(
+        """
+            GIVEN repository throws exception
+            WHEN I invoke this use case
+            THEN I expect a RuntimeException to be propagated
+        """
+    )
+    fun testThrowsException() = testScope.runTest {
+        // GIVEN
+        val page = 3
+        val exception = RuntimeException("Network error")
+        coEvery { repository.getPopularMovies(page) } throws exception
+
+        // WHEN & THEN
+        val thrown = assertThrows<RuntimeException> {
+            classUnderTest(page).first()
+        }
+        assertEquals("Network error", thrown.message)
+    }
+
+}
